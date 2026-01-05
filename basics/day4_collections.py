ff5d84e7047fae9b2bf337b4503bcdf70ba9ad1a
Aditya Rathi <aditya.rathi.work@gmail.com>
2026-01-06 00:38:16 +0530

    Day4 Lists

--- basics/day4_collections.py
+++ basics/day4_collections.py
@@ -42,11 +42,59 @@ if 'banana' in my_list3:
 
 my_list3[1] = 'blueberry'  # changing the second item
 print(my_list3)
+
 my_list3[2:4] = ['coconut', 'dragonfruit']  # changing items from index 2 to 3
 print(my_list3)
+
 my_list3.insert(2, 'blackberry')  # inserting 'blackberry' at index 2
 print(my_list3)
+
 my_list3.append('honeydew')  # adding 'honeydew' at the end
 print(my_list3)
+
 my_list3.extend(['kiwi', 'lemon'])  # adding multiple items at the end
 print(my_list3)
+
+thislist=['andy', 'bob', 'charlie']
+thislist[1:3]=['randy']
+print(thislist)
+
+#diiference between append and extend ad insert
+
+list1=['a','b','c']
+print(list1)
+
+list1.append('de')
+print(list1)  # ['a', 'b', 'c', 'de']
+print(len(list1))
+
+list1.insert(2,'ef')
+print(list1)  
+print(list1[1:3])  # ['b', 'ef']
+
+list1.extend('de')
+print(list1)  # ['a', 'b', 'c', 'd', 'e']
+print(len(list1))
+
+str="xyz"
+lst2=["xyz"]
+list1.extend("xyz")
+list1.append(str)
+list1.extend(lst2)
+print(list1)  # ['a', 'b', 'c', 'd', 'e', 'x', 'y', 'z']
+# extend adds each element of the iterable individually to the end of the list
+
+#_______________________________________________________#
+
+#Removing List Items
+
+list3=['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+list3.remove('date')  # removes 'date'
+print(list3)
+
+#return removes the first instance of the specified value
+
+list3.pop()  # removes the last item
+print(list3)
+list3.pop(1)  # removes item at index 1
+print(list3)
